a3061c4a67af8b3749dca52baf65e4427f895f7b
sprlptr48 <63254295+sprlptr48@users.noreply.github.com>
2022-11-10 22:45:22 +0300

    eNfazla

--- code/b.c
+++ code/b.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 
+int buyuk(int a, int b);
+
 int main(void)
 {
     int marka, tutar;
+    int buy1 = 0, buy2 = 0;
     int ford=0, fordt=0, reno=0, renot=0, mazda=0, mazdat=0, honda=0, hondat=0;
     while (marka != 111)
     {
@@ -19,5 +22,46 @@ int main(void)
             case 111: break;
             default:printf("1234 olmalı");
         }
+    }
+    int buy1 = buyuk(reno, mazda);
+    int buy2 = buyuk(honda, ford);
+    if (buy1 < buy2)
+    {
+        if (buy2 == honda)
+        {
+            printf("En fazla gelen araç markası: Honda");
+        }
+        else if (buy2 == ford)
+        {
+            printf("En fazla gelen araç markası: Ford");
+        }
+    }
+    else
+    {
+        if (buy1 == reno)
+        {
+            printf("En fazla gelen araç markası: Reanult");
+        }
+        else if (buy2 == mazda)
+        {
+            printf("En fazla gelen araç markası: Mazda");
+        }
+
+    }
+}
+
+int buyuk(int a, int b)
+{
+    if (a > b)
+    {
+        return a;
+    }
+    if (a < b)
+    {
+        return b;
+    }
+    else
+    {
+        return 0;
     }
 }
