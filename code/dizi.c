310b3a95facdf226b35b2d5a7f846807773f93e2
sprlptr48 <63254295+sprlptr48@users.noreply.github.com>
2022-11-30 16:51:01 +0300

    diziler

---
+++ code/dizi.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+//main 5 int dizi
+//ortbul(int[x])
+
+double orthesapla(int dizi[5])
+{
+    double ort;
+    for (int i = 0; i < 5; i++)
+    {
+        ort += dizi[i];
+    }
+    ort /= 5.0;
+    return ort;
+}
+
+int main(void)
+{
+    double ort;
+    int saylar[5];
+    for (int i = 0; i < 5; i++)
+    {
+        printf("%d. Sayi: ", i+1);
+        scanf("%d", &saylar[i]);
+        ort = orthesapla(saylar);
+    }
+    printf("Ortalama: %.2f", ort);
+
+}
